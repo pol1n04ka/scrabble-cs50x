a407cbf2541f5c58656f611c3583b14e5e12339f
Polina Prokopenko <topolina1337@gmail.com>
2024-12-30 17:26:24 +0100

    done, but without checks for corner cases

--- scrabble.c
+++ scrabble.c
@@ -4,62 +4,53 @@
 
 int CountScore(char word[]);
 
-// define letters and their value
-const char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+#define MAX_WORD_LENGTH 49
+
+// Scores for each letter
 const int lettersScores[] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
                              1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-const size_t sizeLetters = sizeof(letters) / sizeof(letters[0]);
-const size_t sizeLettersScores =
-    sizeof(lettersScores) / sizeof(lettersScores[0]);
 
 int main(void) {
-
   // define players and words
-  char playerFirst[] = "Player 1: ";
-  char playerSecond[] = "Player 2: ";
+  char playerFirst[] = "Player 1, ";
+  char playerSecond[] = "Player 2, ";
 
-  int playerFirstScore, playerSecondScore;
-
-  char firstWord[50], secondWord[50];
+  char firstWord[MAX_WORD_LENGTH + 1], secondWord[MAX_WORD_LENGTH + 1];
 
   // ask each player for word input
   int checkOne, checkTwo;
 
-  printf("%s", playerFirst);
+  printf("%senter your word (maximum length is %i): ", playerFirst,
+         MAX_WORD_LENGTH);
   checkOne = scanf("%s", firstWord);
   printf("\n");
 
-  printf("%s", playerSecond);
+  printf("%senter your word (maximum length is %i): ", playerSecond,
+         MAX_WORD_LENGTH);
   checkTwo = scanf("%s", secondWord);
   printf("\n");
-
-  printf("%s %s", firstWord, secondWord);
 
   // verify input
 
   // count score for each word
-  playerFirstScore = CountScore(firstWord);
-  playerSecondScore = CountScore(secondWord);
+  int playerFirstScore = CountScore(firstWord);
+  int playerSecondScore = CountScore(secondWord);
 
   printf("First word score is: %i, second word score is: %i\n",
          playerFirstScore, playerSecondScore);
 
   // print who wins
-  /*printf("Someone wins\n");*/
+  printf("\n");
 
-  if (playerFirstScore > playerSecondScore)
-  {
+  if (playerFirstScore > playerSecondScore) {
     printf("Player 1 wins!\n");
-  }
-  else if (playerFirstScore < playerSecondScore)
-  {
+  } else if (playerFirstScore < playerSecondScore) {
     printf("Player 2 wins!\n");
-  } 
-  else 
-  {
-    printf("Tie!\n");
+  } else if (playerFirstScore == 0 || playerSecondScore == 0) {
+    printf("Wrong input, try again!\n");
+    return 1;
+  } else {
+    printf("It's a tie!\n");
   }
 
   return 0;
@@ -70,20 +61,13 @@ int CountScore(char word[]) {
   char *ptr = word;
   int score = 0;
 
-  while (*ptr != '\0') {
+  while (*ptr != '\0' && isalpha((int)*ptr)) {
     char letter = toupper(*ptr);
 
-    for (int i = 0; i < (int)sizeLetters; i++) {
-      if (letter == letters[i]) {
-        score += lettersScores[i];
-        /*printf("Letter: %c, score: %i \n", letter, lettersScores[i]);*/
-      }
-    }
+    score += lettersScores[letter - 'A'];
 
     ptr++;
   }
-
-  /*printf("Score is: %i\n", score);*/
 
   return score;
 }
